f0afe050906aa2c8ca4fb2b84ac8ac0cb296ae21
bcordova <bmcordova@gmail.com>
2011-03-26 10:45:31 -0400

    fixed db_connect to work with localhost

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,13 @@
 <?php
-	$db = mysqli_connect('localhost', 'RAdmin', 'umwresearch2011', 'mwcpscor_trinkleDB')
-		or die ("ERROR: connecting to mysql server!");
-	
-	
+//	$db = mysqli_connect('localhost', 'RAdmin', 'umwresearch2011', 'mwcpscor_trinkleDB')
+//		or die ("ERROR: connecting to mysql server!");
+?>
+
+<?php
+$link = mysql_connect('localhost', 'root', '');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+//echo 'Connected successfully';
+mysql_close($link);
 ?>
